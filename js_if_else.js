79a0a8571f875ce6374e35aa82d88da3bb6ff0b7
Anis Merchant <anisxmerchant@gmail.com>
2018-06-01 10:10:02 -0400

    included an additional if else code example using function

--- js_if_else.js
+++ js_if_else.js
@@ -9,3 +9,14 @@ function shouldWePartyTonight(dayOfTheWeek) {
 };
 
 shouldWePartyTonight('Monday');
+
+function testNum(a) {
+  if (a > 0) {
+    return "positive";
+  } else {
+    return "NOT positive";
+  }
+}
+
+console.log(testNum(-5));
+// expected output: "NOT positive"
