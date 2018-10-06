3069737f5b16005418c080b8eb2cb14642928e82
Anis Merchant <anisxmerchant@gmail.com>
2018-10-06 01:35:59 -0400

    exercise: Complete part one javascript basics activity

---
+++ brainstation/web-dev-ft/ft-course/week-two/activities/programmingBasics/programming_activity_part_1.js
@@ -0,0 +1,28 @@
+/* Follow the instructions below */
+
+// e.g. Print out the string 'Hello World'
+console.log('Hello World')
+
+// Create a variable called 'a' with the value of 1
+var a = 1;
+
+// Check the type of 'a' using 'typeof'. Use console.log() to see the result
+console.log(typeof(a));
+
+// Create another variable called 'b' with the value of 2
+var b = 2;
+
+// Create another variable called 'sum' that is
+// the result of adding 'a' and 'b' together
+var sum = a + b;
+
+// Print out the sum
+console.log(sum);
+// Print out the value of 10 divided by 3
+console.log(10/3);
+// Print out the remainder of 10 divided by 3 using %
+console.log(10%3);
+// Print out the value of 1 + '1'. 
+var string = 1 + '1';
+// What is the result? Why? How could you get the expected result?
+console.log(string);
