bc49981f29325da3024937505f1fc4b671123e70
Anis Merchant <anisxmerchant@gmail.com>
2018-10-16 00:00:49 -0400

    exercise: Print numbers one through four in a staggered pattern challenge

--- brainstation/web-dev-ft/ft-course/week-4/activities/loopsChallenges/script.js
+++ brainstation/web-dev-ft/ft-course/week-4/activities/loopsChallenges/script.js
@@ -44,6 +44,7 @@ Create a loop that goes through an array of numbers and returns the value of the
 You can use the example arrays from Exercise 1 to test your loop.
 */
 
+/*
 console.log("LARGEST NUMBERS - arr1");
 const arr3 = [5,8,2,1,5,7,3,4,5,8,1,2,4,8,3,1,4,5]; // should log 8,2,4,8,2,4,8,4
 const arr4 = [15,26,74,12,3,6,9,1,2,5]; // should log 26,74,12,6,2
@@ -59,3 +60,22 @@ for (i = 0; i < x.length; i++) {
     }
 }
 console.log(largestNumber);
+*/
+/*
+Number triangle
+Create a loop that prints the integers from 1 to 4 in separate rows like the following:
+
+ 1
+ 22
+ 333
+ 4444
+ 
+*/
+
+for (x=0; x<=4; x++) {
+    var string = "";
+    for (y=0; y<x; y++){
+        string += x.toString();        
+    }
+    console.log(string);
+}
