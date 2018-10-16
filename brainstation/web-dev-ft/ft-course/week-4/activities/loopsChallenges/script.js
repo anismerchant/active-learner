e82b2d2ebfed3cacd33c6b1c6d74a5d49a850a77
Anis Merchant <anisxmerchant@gmail.com>
2018-10-15 23:56:17 -0400

    exercise: Complete find the largest number in an array challenge

--- brainstation/web-dev-ft/ft-course/week-4/activities/loopsChallenges/script.js
+++ brainstation/web-dev-ft/ft-course/week-4/activities/loopsChallenges/script.js
@@ -9,7 +9,7 @@ const arr2 = [15,26,74,12,3,6,9,1,2,5]; // should log 26,74,12,6,2
 
 const arr1 = [5,8,2,1,5,7,3,4,5,8,1,2,4,8,3,1,4,5]; // should log 8,2,4,8,2,4,8,4
 const arr2 = [15,26,74,12,3,6,9,1,2,5]; // should log 26,74,12,6,2
-
+/*
 console.log("EVEN NUMBERS - arr1");
 for (index = 0; index < arr1.length; index++) {
     if (arr1[index]%2 === 0) {
@@ -37,3 +37,25 @@ for (i = 0; i < arr1.length; i++) {
         console.log(arr1[i]);
     }
 }
+*/
+/*
+Largest number
+Create a loop that goes through an array of numbers and returns the value of the largest number in the array. 
+You can use the example arrays from Exercise 1 to test your loop.
+*/
+
+console.log("LARGEST NUMBERS - arr1");
+const arr3 = [5,8,2,1,5,7,3,4,5,8,1,2,4,8,3,1,4,5]; // should log 8,2,4,8,2,4,8,4
+const arr4 = [15,26,74,12,3,6,9,1,2,5]; // should log 26,74,12,6,2
+
+x = arr4
+var largestNumber=0;
+
+for (i = 0; i < x.length; i++) {
+    if (largestNumber >= x[i]) {
+        largestNumber += 0;
+    } else {
+        largestNumber = x[i];
+    }
+}
+console.log(largestNumber);
