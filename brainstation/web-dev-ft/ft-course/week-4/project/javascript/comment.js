4bf70958ed4976b009b7e28db359f5487fe2bd22
Anis Merchant <anisxmerchant@gmail.com>
2018-10-18 23:23:26 -0400

    project four: Implement js to grab user input values and display them in comment section

---
+++ brainstation/web-dev-ft/ft-course/week-4/project/javascript/comment.js
@@ -0,0 +1,43 @@
+var commentForm = document.getElementById('grabComments');
+        
+// commentForm.onsubmit = function(e) {
+//     e.preventDefault();    
+//     alert('Form was submitted!');
+// }
+
+commentForm.addEventListener("submit", function(e) {
+    e.preventDefault();    
+    alert('Form was submitted!');
+    getVal();
+    displayVal();
+});
+
+let names = [];
+let comments = [];
+
+function getVal() {
+    let userName = document.getElementById('name').value;
+    let userComment = document.getElementById('comment').value;
+    names.push(userName);
+    comments.push(userComment);    
+}
+
+function displayVal() {
+    for (i=0; i<names.length; i++) {
+        let commentSection = document.getElementById("comments__container");
+        
+        let nameElement = document.createElement('div');
+        let spanElement = document.createElement('span');
+
+        nameElement.appendChild(spanElement);
+        commentSection.appendChild(nameElement);
+        spanElement.innerHTML = names[i];
+        
+        let commentElement = document.createElement('div');
+        let paragraphElement = document.createElement('p');
+
+        commentElement.appendChild(paragraphElement);
+        commentSection.appendChild(commentElement);
+        paragraphElement.innerHTML = comments[i];        
+    }
+}
