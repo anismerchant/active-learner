1daa34f6e593ccafb31bd0053c2c0e7475db8799
Anis Merchant <anisxmerchant@gmail.com>
2018-10-24 23:37:55 -0400

    project five: Automate rows added to shows table based on objects retrieved from server

--- brainstation/web-dev-ft/ft-course/week-5/project/javascript/shows.js
+++ brainstation/web-dev-ft/ft-course/week-5/project/javascript/shows.js
@@ -1,5 +1,4 @@
 const promise = fetch(baseUrl + showsPath + queryString);
-const rows = 6;
 
 // Parent within which all constructed elements are placed
 let showsSection = document.getElementById('shows');
@@ -10,7 +9,7 @@ let showsSection = document.getElementById('shows');
  showsTable.className = 'shows__table';
 
 // Builds a single table with rows
-function buildTableRows(elementSymbols, tableClassNames, data) {
+function buildTableRows(elementSymbols, tableClassNames, data, rows) {
     for (let i=0; i<rows; i++) {
         // Primary row to hold shows details
         let showsTableRows = document.createElement(elementSymbols[0]);
@@ -67,5 +66,6 @@ promise.then((response) => {
             "shows__table__rows--venue",
             "last"
         ];
-        return buildTableRows(elemSymbols, tbClassNames, data);
+        rows = (data.length);
+        return buildTableRows(elemSymbols, tbClassNames, data, rows);
 }); 
