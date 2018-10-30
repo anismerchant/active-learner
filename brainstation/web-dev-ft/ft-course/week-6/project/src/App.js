30ffdf5dd73744822b8fca2cde4c08d478ea45f0
Anis Merchant <anisxmerchant@gmail.com>
2018-10-29 23:24:10 -0400

    project six: Create preliminary navbar component

--- brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
@@ -6,22 +6,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar />
       </div>
     );
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navigation">
+        <div className="navigation__logo--left">
+          <img src="" alt = "Video Player Button"/>
+          <a href="./App.js" target="_self">BrainFlix</a>
+        </div>
+        <form id="navigation__search">
+          <div id="navigation__search--input">
+            <input type="text" name="name" id="search" placeholder="Search" />
+          </div>
+          <button id="navigation__search--icon"></button>
+        </form>
+        <div className="bgImage__navigation--right">
+          <img src="" alt="Camera"/>
+          <img src="" alt="User Thumbnail Image"/>
+        </div>            
+      </nav>  
+    )
   }
 }
 
