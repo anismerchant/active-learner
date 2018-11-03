1a91986561bc6b4a4dd16a045a965ec56dea00f0
Anis Merchant <anisxmerchant@gmail.com>
2018-11-02 22:37:33 -0400

    project six: Create navbar component

---
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/NavBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav id="navigation__container">
+                <div className="navigation__logo">
+                    <img src={"./assets/Icons/BrainFlix Logo.svg"} alt = "BrainFlix Logo"/>
+                </div>
+                <div className="navigation__search">
+                    <input className="navigation__search--input" type="text" placeholder="Search"/>
+                    <div className="navigation__search--icon">
+                        <img src={"./assets/Icons/Search.svg"} />
+                    </div>
+                </div>
+                <div className="navigation__images">
+                    <img className="navigation__images--content-upload" src={"./assets/Icons/Content Upload.svg"} alt="Content Upload"/> 
+                    <img className="navigation__images--john-gibbons" src={"./assets/Images/john_gibbons.jpg"}/>
+                </div>      
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
