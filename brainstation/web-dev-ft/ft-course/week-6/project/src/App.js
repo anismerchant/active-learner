fdc92f026a19ef7a14f9b74c234c9841c98e1e04
Anis Merchant <anisxmerchant@gmail.com>
2018-11-03 00:09:30 -0400

    project six: Import navbar and main section into app component

--- brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './css/style.css';
 import NavBar from './NavBar';
+import MainSection from './MainSection';
 
 class App extends Component {
   render() {
     return (
       <div className="App" id="container">
         <NavBar />
+        <MainSection />
       </div>
     );
   }
