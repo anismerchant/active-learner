ccb424718ebc407603b71b721c447c9cc7982356
Anis Merchant <anisxmerchant@gmail.com>
2018-11-02 23:50:20 -0400

    project six: Add missing 'alt' attributes and '/' in img tags

--- brainstation/web-dev-ft/ft-course/week-6/project/src/NavBar.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/NavBar.js
@@ -5,17 +5,17 @@ class NavBar extends Component {
         return (
             <nav id="navigation__container">
                 <div className="navigation__logo">
-                    <img src={"./assets/Icons/BrainFlix Logo.svg"} alt = "BrainFlix Logo"/>
+                    <img src={"./assets/Icons/BrainFlix Logo.svg"} alt="BrainFlix Logo" />
                 </div>
                 <div className="navigation__search">
-                    <input className="navigation__search--input" type="text" placeholder="Search"/>
+                    <input className="navigation__search--input" type="text" placeholder="Search" />
                     <div className="navigation__search--icon">
-                        <img src={"./assets/Icons/Search.svg"} />
+                        <img src={"./assets/Icons/Search.svg"} alt="Search Bar Icon" />
                     </div>
                 </div>
                 <div className="navigation__images">
-                    <img className="navigation__images--content-upload" src={"./assets/Icons/Content Upload.svg"} alt="Content Upload"/> 
-                    <img className="navigation__images--john-gibbons" src={"./assets/Images/john_gibbons.jpg"}/>
+                    <img className="navigation__images--content-upload" src={"./assets/Icons/Content Upload.svg"} alt="Content Upload" /> 
+                    <img className="navigation__images--john-gibbons" src={"./assets/Images/john_gibbons.jpg"} alt="Author" />
                 </div>      
             </nav>
         );
