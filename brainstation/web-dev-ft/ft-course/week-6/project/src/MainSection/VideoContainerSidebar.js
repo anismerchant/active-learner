6bc0861bd2d7453b2151bd0d8dddc8db4a518082
Anis Merchant <anisxmerchant@gmail.com>
2018-11-04 12:09:35 -0500

    project six: Refactor video container sidebar component

--- brainstation/web-dev-ft/ft-course/week-6/project/src/MainSection/VideoContainerSidebar.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/MainSection/VideoContainerSidebar.js
@@ -1,92 +1,20 @@
 import React, { Component } from 'react';
+import SidebarVideo from './SidebarVideo';
+import SidebarVideoSectionHeading from './SidebarVideoSectionHeading';
 
 class VideoContainerSidebar extends Component {
     render() {
         return (
-            <div className="video__container__sidebar">
-            <div className="video__container__sidebar--heading">
-                <h6 className="video__container__sidebar--upnext">Up next</h6>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/hqdefault.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/donaldson.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/r241851_600x400_3-2.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/PR6AGOQ7XREI5B7UMKM3KAGWFA.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/920x920.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/Big-Read-Vladimir-Guerrero-Jr-Swings-470x264.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/BASEBALL-MLB-HOU-LAD-.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
-            <div className="video__container__sidebar--video">
-                <div className="video__container__sidebar--video-artifact">
-                    <img src="./assets/Images/THKMOYWFLWJCPXQ.20170430201114.jpg" alt="Video" />
-                </div>
-                <div className="video__container__sidebar--video-info">
-                    <h6 className="video__container__sidebar--text">TEX @TOR Gm5: Blue Jays take lead in wild 7th inning</h6>
-                    <h6 className="video__container__sidebar--name">MLB</h6>
-                    <h6 className="video__container__sidebar--views">1.1M Views</h6>
-                </div>
-            </div>
+        <div className="video__container__sidebar">
+            <SidebarVideoSectionHeading />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
+            <SidebarVideo />
         </div>
         );
     }
