557fa4bfeabade2b4990ccb0280335cde44627ab
Anis Merchant <anisxmerchant@gmail.com>
2018-11-04 12:45:36 -0500

    project six: Create main video heading component

---
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/MainSection/MainVideoHeading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainVideoHeading extends Component {
+    render() {
+        return (
+            <div className="video__container__main--text">
+                <h4>Jose Bautista hammers go-ahead three-run shot in ALDS Game 5, delivers epic bat flip</h4>
+            </div>
+        );
+    }
+}
+
+export default MainVideoHeading;
