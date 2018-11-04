18e5ea2dfe01332b3fc7cea9fdbdbe70705d59a5
Anis Merchant <anisxmerchant@gmail.com>
2018-11-04 14:21:11 -0500

    project six: Update import paths

--- brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './css/style.css';
-import NavBar from './containers/NavBar';
+import NavBarSection from './containers/NavBarSection';
 import MainSection from './containers/MainSection';
 
 class App extends Component {
   render() {
     return (
       <div className="App" id="container">
-        <NavBar />
+        <NavBarSection />
         <MainSection />
       </div>
     );
