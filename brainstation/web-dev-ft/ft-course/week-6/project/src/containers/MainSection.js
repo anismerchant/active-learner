c0aa4615e65037cbcbedb7b28d3e6ec18bac80de
Anis Merchant <anisxmerchant@gmail.com>
2018-11-04 22:10:46 -0500

    project six: Import videos data and define state

--- brainstation/web-dev-ft/ft-course/week-6/project/src/containers/MainSection.js
+++ brainstation/web-dev-ft/ft-course/week-6/project/src/containers/MainSection.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import videos from '../videoData';
 import VideoContainerMain from '../components/MainSection/VideoContainerMain';
 import VideoContainerSidebar from '../components/MainSection/VideoContainerSidebar';
 
 class MainSection extends Component {
+    state = {
+        videos: videos
+      }
+
     render() {
         return(
             <main id="video__container">
-                <VideoContainerMain />
-                <VideoContainerSidebar />
+                <VideoContainerMain videoArray={this.state.videos} />
+                <VideoContainerSidebar videoArray={this.state.videos} />
             </main>
         );
     }
