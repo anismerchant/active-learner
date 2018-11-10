d956b097aefac771976f236912629566eebad9d2
Anis Merchant <anisxmerchant@gmail.com>
2018-11-10 00:27:34 -0500

    project seven: Refactor sidebar video component to filter out clicked videos

--- brainstation/web-dev-ft/ft-course/week-7/project/src/components/MainSection/SidebarVideoContainer.js
+++ brainstation/web-dev-ft/ft-course/week-7/project/src/components/MainSection/SidebarVideoContainer.js
@@ -4,27 +4,30 @@ import { Link } from 'react-router-dom'
 
 class SidebarVideoContainer extends Component {
     render() {
-        let videos = this.props.videoArray;
+        let videos = this.props.videoObject;
+        let {match} = this.props;
             return (
-            <div className="video__container__sidebar">
-                <div className="video__container__sidebar--heading">
-                    <h6 className="video__container__sidebar--upnext">Up next</h6>
+                <div className="video__container__sidebar">
+                    <div className="video__container__sidebar--heading">
+                        <h6 className="video__container__sidebar--upnext">Up next</h6>
+                    </div>
+                    { videos.filter((removeSelectedVid) => {
+                        return match.params.id !== removeSelectedVid.id
+                    }).map((video, index) => {  
+                        return <Link className="video__container__sidebar--video-link-container" to={`/videos/${video.id}`}
+                                key={index}>
+                                    <SidebarVideo 
+                                        key={index}
+                                        title={video.title}
+                                        channel={video.channel}
+                                        views={video.views}
+                                        duration={video.duration}
+                                        image={video.image}
+                                    />
+                                </Link>
+                        })
+                    }
                 </div>
-                { videos.map ( (video, index) => {
-                    return <Link className="link" to={`/videos/${video.id}`}
-                            key={index}>
-                        <SidebarVideo 
-                            key={index}
-                            title={video.title}
-                            channel={video.channel}
-                            views={video.views}
-                            duration={video.duration}
-                            image={video.image}
-                        />
-                    </Link>
-                    })
-                }
-            </div>
             );
         }
     }
