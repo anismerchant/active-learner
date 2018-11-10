619420144f007fa021f6030e2a7766578358c755
Anis Merchant <anisxmerchant@gmail.com>
2018-11-10 00:45:29 -0500

    project seven: Grab comments objects from brainflix API

--- brainstation/web-dev-ft/ft-course/week-7/project/src/components/MainSection/MainVideoContainer.js
+++ brainstation/web-dev-ft/ft-course/week-7/project/src/components/MainSection/MainVideoContainer.js
@@ -3,18 +3,26 @@ import MainVideo from './MainVideo';
 
 class MainVideoContainer extends Component {
     render() {
-        let videos = this.props.videoDetailsArray;
+        let videos = this.props.videoDetailsObject;
+         
         return (
             <MainVideo 
                mainvideo={`${videos.video}${this.props.apiKey}`}
                poster={videos.image}
                titlemain={videos.title}
+               description={videos.description}
                viewsmain={videos.views}
                thumbsup={videos.thumbsUp}
                thumbsdown={videos.thumbsDown}
                channel={videos.channel} 
                subscribe={videos.subscriberCount}
+               comments={
+                   videos.comments.map((comment) => {
+                        return comment;
+                    })
+               }
             />
+
         );
     }
 }
