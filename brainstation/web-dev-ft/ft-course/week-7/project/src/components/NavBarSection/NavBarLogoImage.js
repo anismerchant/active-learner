b07fd3c84389fd76d3b4736f72dedc5716f00af4
Anis Merchant <anisxmerchant@gmail.com>
2018-11-10 17:26:34 -0500

    project seven: Create image components

---
+++ brainstation/web-dev-ft/ft-course/week-7/project/src/components/NavBarSection/NavBarLogoImage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavBarLogoImage extends Component {
+    render() {
+        return (
+            <img src={"../../assets/Icons/BrainFlix Logo.svg"} alt="BrainFlix Logo" />
+        );
+    }
+}
+
+export default NavBarLogoImage;
---
+++ brainstation/web-dev-ft/ft-course/week-7/project/src/components/NavBarSection/NavBarUploadImage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NavBarUploadImage extends Component {
+    render() {
+        return (
+            <img className="navigation__images--content-upload" src={"../../assets/Icons/Content Upload.svg"} alt="Content Upload" /> 
+        );
+    }
+}
+
+export default NavBarUploadImage;
